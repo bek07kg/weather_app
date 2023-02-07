2d45af9181dd1c32ac7e7b8d53354b76525bab78
bek07kg <bekamurzaev4@gmail.com>
2023-02-07 14:17:53 +0300

    ui

---
+++ lib/model/weather.dart
@@ -0,0 +1,12 @@
+class Weather {
+  final String main;
+  final String description;
+  final String icon;
+  final String name;
+
+  Weather(
+      {required this.main,
+      required this.description,
+      required this.icon,
+      required this.name});
+}
